b62e153682fce45ab2e92f6b5f734fbfee1e67ab
git config --global user.name <islamhovenwork@gmail.com>
2025-05-08 18:00:12 +0300

    Fix image paths

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -7,26 +7,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { clearUserInfo } from "../../Redux/Slice/UserSlice";
 
 function Header() {
-  const user = useSelector((state)=>state.User.userInfo)
-  const userDone = useSelector((state)=>state.User.userDone)
-  const doneStatue = useSelector((state)=>state.User.doneStatue)
+  const user = useSelector((state) => state.User.userInfo);
+  const userDone = useSelector((state) => state.User.userDone);
+  const doneStatue = useSelector((state) => state.User.doneStatue);
   const userClass = doneStatue ? 'userName-active' : 'userName-inactive';
-  const signClass = doneStatue? 'signClass-inactive':'signClass-active';
+  const signClass = doneStatue ? 'signClass-inactive' : 'signClass-active';
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const logout=()=>{
-      dispatch(clearUserInfo())
-      alert(`Good Bye ! See you later ${userDone.userName} 💝`)
-      navigate('/')}
+
+  const logout = () => {
+    dispatch(clearUserInfo());
+    alert(`Good Bye ! See you later ${userDone.userName} 💝`);
+    navigate('/');
+  };
 
   return (
     <Navbar expand="lg" bg="light" variant="light" sticky="top" style={{ height: "80px" }}>
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <img 
-            src={logo} 
-            alt="UK FORMIFY Logo" 
-            style={{ maxHeight: "50px",maxWidth:"200px", objectFit: "contain " }}  
+          <img
+            src={logo}
+            alt="UK FORMIFY Logo"
+            style={{ maxHeight: "50px", maxWidth: "200px", objectFit: "contain" }}
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
@@ -37,19 +39,17 @@ function Header() {
             <Nav.Link as={Link} to="/about">About Us</Nav.Link>
             <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
             <Nav.Link as={Link} to="/ContactUS">Contact</Nav.Link>
-            
 
             <div className={`signClass ${signClass}`}>
-        <Nav.Item>
-        <Nav.Link as={Link} className="CTA" to="/signup">Start NOW!</Nav.Link>
-        </Nav.Item>
-        </div>
+              <Nav.Item>
+                <Nav.Link as={Link} className="CTA" to="/signup">Start NOW!</Nav.Link>
+              </Nav.Item>
+            </div>
 
-        <div className={`userName ${userClass}`}>
-        <Nav.Link as={Link} to="/">{user ? ` ${userDone.userName}` : ""}</Nav.Link>
-
-        <i onClick={logout} className="fas fa-sign-out-alt"></i>
-      </div>
+            <div className={`userName ${userClass}`}>
+              <Nav.Link as={Link} to="/">{user ? ` ${userDone.userName}` : ""}</Nav.Link>
+              <i onClick={logout} className="fas fa-sign-out-alt"></i>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
