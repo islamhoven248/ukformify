d07301c846c8d8d0c8ff8b7aa11a8cebf4ecb615
git config --global user.name <islamhovenwork@gmail.com>
2025-05-08 17:10:50 +0300

    modified:   src/Pages/Ourplans.jsx

--- src/Pages/Ourplans.jsx
+++ src/Pages/Ourplans.jsx
@@ -1,3 +1,31 @@
-git add .
-git commit -m "Fix file path for Plans.json"
-git push origin main
+import React from 'react';
+import { Container, Table } from 'react-bootstrap';
+import plans from '../data/Plans.json'; 
+
+export default function Ourplans() {
+  return (
+    <Container className="py-5">
+      <Table striped bordered hover responsive>
+      <thead>
+              <tr>
+                <th>Feature</th>
+                <th>Starter</th>
+                <th>Business</th>
+                <th>Premium</th>
+              </tr>
+            </thead>
+        <tbody>
+          {plans.map((plan, index) => (
+            <tr key={index}>
+              <td>{plan.title}</td>
+              <td>{plan.priceEG}</td>
+              <td>{plan.priceER}</td>
+              
+              <td>{plan.desc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+}
